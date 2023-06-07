7461125389b13e040910d6cd6747d18880a57bdc
Rajashree Pethkar <rpethkar@pdx.edu>
2023-06-07 06:43:57 -0700

    Added baseurl to axios

--- src/utils/gameStats.jsx
+++ src/utils/gameStats.jsx
@@ -8,7 +8,12 @@ const headers = {
 
 async function getTeamGameStats(week) {
   try {
-    const response = await axios.get(nflUrl.concat(week), { headers });
+    const api = axios.create({
+      baseURL: import.meta.env.VITE_NFL_URL,
+    });
+    const response = await api.get(nflUrl.concat(week), {
+      headers,
+    });
     return response.data;
   } catch (err) {
     console.error(err);
